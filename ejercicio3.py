e171a134d70070d6f68a009db34d993ea2951998
OmairaBarrera <lomaira.barrera@gmail.com>
2023-02-17 13:32:56 -0500

    Avance2 ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -11,12 +11,35 @@ kilómetros se consideran especiales y tendrán un recargo de
 El total de kilómetros por recorrer durante toda la vuelta serán
 3.277 kilómetros,el ganador de la vuelta a España recibirá 700
 millones de pesos. """
+import json
 
+def ingresarCiclista(ciclistas):
+    j=0
+    while j != ciclistas:
+        nombre=input("Ingrese el nombre del ciclista: ")
+        sueldo=int(input("Ingrese sueldo basico por kilometro recorrido: "))
+        numeroK=int(input("Ingrese Kilometros recorridos: "))
+        numeroKL=int(input("Ingrese Kilometros camiseta Lider: "))
+        datos=[sueldo,numeroK,numeroKL]
+        ciclistasBase[nombre]=datos
+        j+=1
+    print(json.dumps(ciclistasBase, indent=4))
+
+def mostrarCiclistas():
+    i=1
+    print("LISTA DE CICLISTAS")
+    for x in ciclistasBase.keys():
+        print(f'CICLISTA {i}')
+        print(f'Nombre= {x}')
+        print(f'Sueldo por Kilometro= {ciclistasBase[x][0]}')
+        print(f'Kilometros recorridos= {ciclistasBase[x][1]}')
+        print(f'Kilometros camiseta Lider= {ciclistasBase[x][2]}')
+        i+=1
 #menu
 i=0
 cont=0
-finalistasNombre=[]
-finalistasMarca=[]
+ciclistasBase={}
+datos=[]
 while i==0:
     print(f'----------MENU-----------')
     print(f'1. INGRESAR CANTIDAD DE CICLISTAS')
